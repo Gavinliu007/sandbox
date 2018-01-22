51b25bc56540e03e46b3d926e27841972792b848
Gavin Liu <gavinliu007@gmail.com>
2018-01-21 16:50:12 -0800

    Init checkin

---
+++ building_owner_addline1.sql
@@ -0,0 +1,37 @@
+CREATE OR REPLACE FUNCTION "AMANDA"."BUILDING_OWNER_ADDLINE1" (parmFolderRSN number)
+   return varchar2  is
+
+vl_addressline1 varchar2(60);
+
+
+
+v_NAME                 VARCHAR2(50) := null;
+v_ORGANIZATIONNAME      VARCHAR2(50) := null;
+
+begin
+
+     begin
+        select rtrim(p.namefirst)||' '||rtrim(p.namelast), p.organizationname
+        into v_name, v_organizationname
+        from folder f,  property pb,  propertypeople pp, people p
+        where f.folderrsn = parmFolderRSN
+        and pb.propertyrsn(+) = f.propertyrsn
+        and pp.propertyrsn(+) = pb.parentpropertyrsn
+        and p.peoplersn(+) = pp.peoplersn
+        and rownum = 1;
+
+     exception
+	       WHEN NO_DATA_FOUND THEN
+            v_name := null;
+            v_organizationname := null;
+     end;
+
+if v_organizationname is not null then
+   vl_addressline1 := v_name;
+else
+   vl_addressline1 := null;
+end if;
+
+return vl_addressline1;
+
+end building_owner_addline1;
